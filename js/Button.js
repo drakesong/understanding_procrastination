0ba28d2e84f1df09b2ea1fe2f861b6dcdd215813
Drake Song <drakesong216@gmail.com>
2018-04-09 12:41:11 -0700

    Add brief outline of rest of the paper and reorganized files

---
+++ js/Button.js
@@ -0,0 +1,50 @@
+(function() {
+
+    function Button(label, color, func) {
+        this.Container_constructor();
+
+        this.color = color;
+        this.label = label;
+        this.func = func;
+
+        this.setup();
+    }
+
+    var p = createjs.extend(Button, createjs.Container);
+
+    p.setup = function() {
+        var text = new createjs.Text(this.label, "20px Sans-serif", "#000");
+        text.textBaseline = "top";
+        text.textAlign = "center";
+
+        var width = text.getMeasuredWidth() + 30;
+        var height = text.getMeasuredHeight() + 20;
+
+        text.x = width / 2;
+        text.y = 10;
+
+        var background = new createjs.Shape();
+        background.graphics.beginFill(this.color).drawRoundRect(0, 0, width, height, 5);
+
+        this.addChild(background, text);
+        this.on("click", this.handleClick);
+        this.on("rollover", this.handleRollOver);
+        this.on("rollout", this.handleRollOver);
+        this.cursor = "pointer";
+
+        this.mouseChildren = false;
+
+        this.offset = Math.random() * 10;
+        this.count = 0;
+    };
+
+    p.handleClick = function(event) {
+        this.func();
+    };
+
+    p.handleRollOver = function(event) {
+        this.alpha = event.type == "rollover" ? 0.4 : 1;
+    };
+
+    window.Button = createjs.promote(Button, "Container");
+}());
